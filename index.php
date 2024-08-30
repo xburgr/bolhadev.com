178eaaa3b5aa953954775c78a783f8d6731e7f76
xburgr <xburgr2020@gmail.com>
2024-08-30 19:24:53 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<title>#bolhadev</title>
+</head>
+<body>
+<h1>#bolahdev</h1>
+  <p>Este é o site da #bolhadev, ele está em contrução.</p>
+  <p>A idéia é dar continuidade a #bolhadev do Twitter.</p>
+  <p>Deixem suas dicas, críticas e sugestões no <a href="https://github.com/xburgr/bolhadev.com/issues">Github</a>.</p>
