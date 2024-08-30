d0b48653de00494185943f05ac1526186ef636ef
xburgr <xburgr2020@gmail.com>
2024-08-30 19:25:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <title>#bolhadev</title>
 </head>
 <body>
-<h1>#bolahdev</h1>
+<h1>#bolhadev</h1>
   <p>Este é o site da #bolhadev, ele está em contrução.</p>
   <p>A idéia é dar continuidade a #bolhadev do Twitter.</p>
   <p>Deixem suas dicas, críticas e sugestões no <a href="https://github.com/xburgr/bolhadev.com/issues">Github</a>.</p>
